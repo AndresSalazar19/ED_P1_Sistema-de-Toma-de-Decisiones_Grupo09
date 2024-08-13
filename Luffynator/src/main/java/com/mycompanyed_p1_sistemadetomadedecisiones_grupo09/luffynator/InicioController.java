101134e98f38245718a2d27ca5adb0adc2ed218d
AndresSalazar19 <aasalazar@espol.edu.ec>
2024-08-12 21:27:32 -0500

    Implementación básica de java fx

---
+++ Luffynator/src/main/java/com/mycompanyed_p1_sistemadetomadedecisiones_grupo09/luffynator/InicioController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompanyed_p1_sistemadetomadedecisiones_grupo09.luffynator;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author asala
+ */
+public class InicioController implements Initializable {
+    
+    @FXML
+    public void switchToSecondary(){
+        System.out.println("jugar");
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
